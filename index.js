2313122fd5cd57430193558169b87e9962d2f86b
Julien Rousseau <julienrousseau16@gmail.com>
2020-03-25 12:14:13 +0100

    submission of "wilders" color work

---
+++ index.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+ 
+console.log(
+    chalk.blue('Julien Rousseau'),
+    chalk.yellow('Tristan Manaut'),
+    chalk.green('Lola Donval'),
+    chalk.red('Nicolas Régnier')
+);
